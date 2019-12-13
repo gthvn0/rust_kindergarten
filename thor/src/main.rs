08ea52f0e816865c763299bfe5186ca38d35e2ed
Guillaume <gthouvenin@kalray.eu>
2019-12-13 13:23:48 +0100

    Use struct instead of tuple
    It allows to use specific method and we can easily deal with only
    destination to compute the next position and reach the final
    destination.

--- thor/src/main.rs
+++ thor/src/main.rs
@@ -9,108 +9,88 @@
 //         |                     |
 // (0,18)  +---------------------+ (40, 18)
 //
-type Point = (i8, i8);
 
-const N: Point  = (0, -1);
-const NE: Point = (1, -1);
-const E: Point  = (1, 0);
-const SE: Point = (1, 1);
-const S: Point  = (0, 1);
-const SW: Point = (-1, 1);
-const W: Point  = (-1, 0);
-const NW: Point = (-1, -1);
+#[derive(Debug, PartialEq, Eq)]
+struct Point {
+    x: i8,
+    y: i8,
+}
 
-fn get_direction(orig: Point, dest: Point) -> (Point, String) {
-    let pt: Point = (compare(orig.0, dest.0), compare(orig.1, dest.1));
-    let dir = match pt {
-        N => String::from("N"),
-        NE => String::from("NE"),
-        E => String::from("E"),
-        SE => String::from("SE"),
-        S => String::from("S"),
-        SW => String::from("SW"),
-        W => String::from("W"),
-        NW => String::from("NW"),
-        _ => String::from("X"), // Don't move :-)
-    };
+const N: Point  = Point {x: 0, y:-1};
+const NE: Point = Point {x: 1, y: -1};
+const E: Point  = Point {x: 1, y: 0};
+const SE: Point = Point {x: 1, y: 1};
+const S: Point  = Point {x: 0, y: 1};
+const SW: Point = Point {x: -1, y: 1};
+const W: Point  = Point {x: -1, y: 0};
+const NW: Point = Point {x: -1, y: -1};
 
-    (pt, dir)
+impl Point {
+    fn add(&self, pt: &Point) -> Point {
+        Point {x: self.x + pt.x, y: self.y + pt.y}
+    }
+
+    fn move_to(&self, dest: &str) -> Point {
+        match dest {
+            "N" =>  self.add(&N),
+            "NE" => self.add(&NE),
+            "E" =>  self.add(&E),
+            "SE" => self.add(&SE),
+            "S" =>  self.add(&S),
+            "SW" => self.add(&SW),
+            "W" =>  self.add(&W),
+            "NW" => self.add(&NW),
+            _ => panic!("PANIC: you are heading to a black hole")
+        }
+    }
+
+    fn path_to(&self, dest: &Point) -> String {
+        let pt = Point {x:compare(self.x, dest.x), y: compare(self.y, dest.y)};
+        match pt {
+            N =>  String::from("N"),
+            NE => String::from("NE"),
+            E =>  String::from("E"),
+            SE => String::from("SE"),
+            S =>  String::from("S"),
+            SW => String::from("SW"),
+            W =>  String::from("W"),
+            NW => String::from("NW"),
+            _ =>  String::from("X"), // Don't move, you reached the destination
+        }
+    }
 }
 
 fn compare(x: i8, y: i8) -> i8 {
-    if x < y {
-        return 1;
-    }
-
-    if x > y {
-        return -1;
-    }
-
+    if x < y { return 1; }
+    if x > y { return -1; }
     0
 }
 
 fn main() {
-    let thor: Point = (5, 4);
-    let light: Point = (31, 4);
+    let thor  = Point {x:5, y: 4};
+    let light = Point {x:31, y: 4};
 
     println!("Start at {:?}", thor);
     println!("Goto {:?}", light);
 
-    let (next_point, direction) = get_direction(thor, light);
-    println!("Next point direction {:?}", next_point);
+    let direction = thor.path_to(&light);
     println!("Go to {}", direction);
+    println!("Thor is now at {:?}", thor.move_to(&direction));
 }
 
-#[test]
-fn test_arrived_destination() {
-    let (_, path) = get_direction((5,5), (5,5));
-    assert_eq!(path, "X");
-}
 
 #[test]
-fn test_go_to_north() {
-    let (_, path) = get_direction((5,5), (5,2));
-    assert_eq!(path, "N");
-}
+fn test_all_destinations() {
+    let start = Point {x:5, y:5};
 
-#[test]
-fn test_go_to_north_east() {
-    let (_, path) = get_direction((5,5), (6,2));
-    assert_eq!(path, "NE");
-}
-
-#[test]
-fn test_go_to_east() {
-    let (_, path) = get_direction((5,5), (8,5));
-    assert_eq!(path, "E");
-}
-
-#[test]
-fn test_go_to_south_east() {
-    let (_, path) = get_direction((5,5), (7,9));
-    assert_eq!(path, "SE");
-}
-
-#[test]
-fn test_go_to_south() {
-    let (_, path) = get_direction((5,5), (5,9));
-    assert_eq!(path, "S");
-}
-
-#[test]
-fn test_go_to_south_west() {
-    let (_, path) = get_direction((5,5), (3,9));
-    assert_eq!(path, "SW");
-}
-
-#[test]
-fn test_go_to_west() {
-    let (_, path) = get_direction((5,5), (3,5));
-    assert_eq!(path, "W");
-}
-
-#[test]
-fn test_go_to_norht_west() {
-    let (_, path) = get_direction((5,5), (3,2));
-    assert_eq!(path, "NW");
+    assert_eq!(start.path_to(&Point{x:5,y:5}), "X");
+    assert_eq!(start.path_to(&Point{x:5,y:2}), "N");
+    assert_eq!(start.path_to(&Point{x:7,y:2}), "NE");
+    assert_eq!(start.path_to(&Point{x:6,y:2}), "NE");
+    assert_eq!(start.path_to(&Point{x:8,y:5}), "E");
+    assert_eq!(start.path_to(&Point{x:7,y:9}), "SE");
+    assert_eq!(start.path_to(&Point{x:5,y:9}), "S");
+    assert_eq!(start.path_to(&Point{x:3,y:9}), "SW");
+    assert_eq!(start.path_to(&Point{x:3,y:5}), "W");
+    assert_eq!(start.path_to(&Point{x:3,y:2}), "NW");
 }
