98806a1255858d5e728b803341555ae1b024e287
gthvn0 <gthvn0@gmail.com>
2019-12-14 14:16:50 +0100

    Implement function compare_0
    This function takes two integer and return the closest to 0. If values
    are at the same "distance" the positive one is returned.

--- temperatures/src/main.rs
+++ temperatures/src/main.rs
@@ -7,11 +7,30 @@ macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
+fn compare_0(x:i32, y:i32) -> i32 {
+    match x + y {
+        0 => x.abs(),
+        _ => if x.abs() < y.abs() {x} else {y}
+    }
+}
+
 fn main() {
     let inputs = String::from("1 -2 -8 4 5");
+    let mut closest:i32 = 256;
 
     for i in inputs.split_whitespace() {
         let t = parse_input!(i, i32);
-        println!("{}", t);
+        closest = compare_0(closest, t);
     }
+
+    println!("{:?}", closest);
+}
+
+#[test]
+fn test_closest() {
+    assert_eq!(compare_0(5, -5), 5);
+    assert_eq!(compare_0(5, 3), 3);
+    assert_eq!(compare_0(-5, -13), -5);
+    assert_eq!(compare_0(7, -15), 7);
+    assert_eq!(compare_0(7, -5), -5);
 }
