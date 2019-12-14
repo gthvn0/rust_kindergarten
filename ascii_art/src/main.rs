a75b8d9013820cd12d0a55d13346546a3e0566e7
gthvn0 <gthvn0@gmail.com>
2019-12-14 18:09:46 +0100

    First commit

---
+++ ascii_art/src/main.rs
@@ -0,0 +1,29 @@
+//
+// l is the width of the letter
+// h is the height of the letter
+// t is the text to represent
+// into row we have the string of characters (ABC..Z?) in ASCII art
+//
+// For exemple:
+// l: 4
+// h: 5
+// t: E
+// row:  #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###
+// row: # # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #
+// row: ### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##
+// row: # # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #
+// row: # # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #
+//
+// should return
+//
+// ###
+// #
+// ##
+// #
+// ###
+
+const NB_LETTERS: i32 = 27;
+   
+fn main() {
+    println!("Hello, world!");
+}
