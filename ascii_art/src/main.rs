0fe6685cbc0df2887abe58abee2dea3a6d21fcad
gthvn0 <gthvn0@gmail.com>
2019-12-15 18:30:59 +0100

    Add display_ascii() and get_slice_for_letter()
    get_slice_for_letter() returns a slice for a letter. The line of the
    ascii vector is passed as parameter. The display_ascii() function is
    calling get_slice_for_letter() for all lines that is the length of the
    ascii art.

--- ascii_art/src/main.rs
+++ ascii_art/src/main.rs
@@ -22,8 +22,47 @@
 // #
 // ###
 
-const NB_LETTERS: i32 = 27;
-   
+fn get_slice_for_letter(c: char, width: usize, ascii: &str) -> &str {
+    let diff = (c.to_ascii_uppercase() as usize) - ('A' as usize);
+    &ascii[diff * width..(diff + 1) * width]
+}
+
+fn display_ascii(slice: &str, width: usize, ascii: &Vec<&str>) {
+    let mut iterator = ascii.iter();
+    loop {
+        match iterator.next() {
+            None => {
+                break
+            },
+            Some(line) => {
+                let mut display = Vec::new();
+                for c in slice.chars() {
+                    display.push(get_slice_for_letter(c, width, line));
+                }
+                println!("{}", display.concat());
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let l = 4;
+    let _h = 5;
+    // A -> 0..l
+    // B -> l..2l
+    // C -> 2l..3l
+    let mut ascii = Vec::new();
+    ascii.push(" #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###");
+    ascii.push("# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #");
+    ascii.push("### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ##");
+    ascii.push("# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #      ");
+    ascii.push("# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  # ");
+
+    let t = "HellO";
+    display_ascii(&t, l, &ascii);
+
+    println!("\n");
+
+    let t = "Manhattan";
+    display_ascii(&t, l, &ascii);
 }
