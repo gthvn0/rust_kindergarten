fe792f86e27e23308d072750b4033b5a3dd0c1b8
gthvn0 <gthvn0@gmail.com>
2019-12-16 15:29:16 +0100

    Fix representation in bits to be on 7 bits
    To work all integer between 0 and 127 must have a representation on 7
    bits. Otherwise you don't speack as chuck norris.

--- chuck_norris/src/main.rs
+++ chuck_norris/src/main.rs
@@ -18,10 +18,18 @@ fn decompose(s: &str) -> Vec<u8> {
     res
 }
 
-// 12 -> [1, 1, 0, 0]
-fn to_binary(x: u8) -> Vec<u8> {
+// binary is returning the ASCII 7-bits representation
+// 12 -> [0, 0, 0, 1, 1, 0, 0]
+fn u8_to_binary(x: u8) -> Vec<u8> {
+    const BITS: usize = 7;
+    const MAXVAL: u8 = 127;
+
+    if x > MAXVAL {
+        panic!("{} is not allowed on 7-bits", x);
+    }
+
     if x == 0 {
-        return vec![0];
+        return vec![0, 0, 0, 0, 0, 0, 0];
     }
 
     let mut v = Vec::new();
@@ -32,8 +40,22 @@ fn to_binary(x: u8) -> Vec<u8> {
         val >>= 1;
     }
 
+    // Padding
+    for _ in v.len()..BITS {
+        v.push(0 as u8);
+    }
+
     v.reverse();
     v
+}
+
+// [12, 6] -> [1, 1, 0, 0, 1, 1, 0]
+fn vec_to_binary(v: &[u8]) -> Vec<u8> {
+    let mut res = Vec::new();
+    for elmt in v {
+        res.extend(u8_to_binary(*elmt));
+    }
+    res
 }
 
 // [1, 1, 0, 0, 0, 1, 1, 0, 1] => [(2, 1), (3, 0), (2, 1), (1, 0), (1, 1)]
@@ -71,15 +93,15 @@ fn speak_chuck(nb: u8, val: u8) -> String {
 fn chuck_translator(s: &str) -> String {
 
     let mut chuck = String::new();
+    let vect = decompose(s);
+    let bin = vec_to_binary(&vect);
+    let trans = transform_vec(&bin);
 
-    for c in s.chars() {
-        let c_ascii = c as u8;
-        let v1 =  to_binary(c_ascii);
-        for (n, v) in transform_vec(&v1) {
-            chuck.push_str(&speak_chuck(n, v));
-            chuck.push(' ');
-        }
+    for (n, v) in trans {
+        chuck.push_str(&speak_chuck(n, v));
+        chuck.push(' ');
     }
+
     // just pop the last spurious space
     chuck.pop().unwrap();
     chuck
@@ -89,21 +111,25 @@ fn main() {
 
     println!("{}", chuck_translator("Hello"));
     println!("{}", chuck_translator("C"));
+    println!("{}", chuck_translator("CC"));
+}
 
-    println!("Hello -> {:?}", decompose("Hello"));
+#[test]
+fn test_chuck_translator() {
+    let hello = String::from("0 0 00 00 0 0 00 000 0 00 00 00 0 0 00 0 0 000 00 0 0 00 00 00 0 00 00 0 0 00 00 00 0 00 00 0 0 0000");
+    let c = String::from("0 0 00 0000 0 00");
+    let cc = String::from("0 0 00 0000 0 000 00 0000 0 00");
+    let percentage = String::from("00 0 0 0 00 00 0 0 00 0 0 0");
 
-    let c_ascii = 'C' as u8;
-    let v1 = to_binary(c_ascii);
-    print!("C: {} ({:b}) -> {:?} -> ", c_ascii, c_ascii, v1);
+    assert_eq!(chuck_translator("Hello"), hello);
+    assert_eq!(chuck_translator("C"), c);
+    assert_eq!(chuck_translator("CC"), cc);
+    assert_eq!(chuck_translator("%"), percentage);
+}
 
-    let trans =  transform_vec(&v1);
-    print!("{:?} -> ", trans);
-
-    for (n, v) in trans {
-        print!("{} ", speak_chuck(n, v));
-    }
-
-    println!("\n");
+#[test]
+fn test_vec_to_binary() {
+    assert_eq!(vec_to_binary(&vec![12, 6]), vec![0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0]);
 }
 
 #[test]
@@ -112,12 +138,14 @@ fn test_decompose() {
 }
 
 #[test]
-fn test_to_binary() {
-    assert_eq!(to_binary(0), vec![0]);
-    assert_eq!(to_binary(1), vec![1]);
-    assert_eq!(to_binary(12), vec![1, 1, 0, 0]);
-    assert_eq!(to_binary(41), vec![1, 0, 1, 0, 0, 1]);
-    assert_eq!(to_binary(118), vec![1, 1, 1, 0, 1, 1, 0]);
+fn test_u8_to_binary() {
+    assert_eq!(u8_to_binary(0),   vec![0, 0, 0, 0, 0, 0, 0]);
+    assert_eq!(u8_to_binary(1),   vec![0, 0, 0, 0, 0, 0, 1]);
+    assert_eq!(u8_to_binary(6),   vec![0, 0, 0, 0, 1, 1, 0]);
+    assert_eq!(u8_to_binary(12),  vec![0, 0, 0, 1, 1, 0, 0]);
+    assert_eq!(u8_to_binary(41),  vec![0, 1, 0, 1, 0, 0, 1]);
+    assert_eq!(u8_to_binary(118), vec![1, 1, 1, 0, 1, 1, 0]);
+    assert_eq!(u8_to_binary(127), vec![1, 1, 1, 1, 1, 1, 1]);
 }
 
 #[test]
