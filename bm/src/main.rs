df190eca93143b97227a618c2f0b7c28d6cb802a
Guillaume <gthouvenin@kalray.eu>
2022-07-05 20:57:36 +0200

    bm: start playing with Yaml

--- bm/src/main.rs
+++ bm/src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused)]
 
+extern crate yaml_rust;
+use yaml_rust::{YamlLoader, YamlEmitter};
+
 use clap::Parser;
 
 #[derive(Parser)]
@@ -18,13 +21,67 @@ struct Args {
     add: Option<String>,
 }
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+fn list_bookmarks(doc: &yaml_rust::yaml::Yaml)
+{
+    // Debug support
+    println!("List bookmarks called");
+    println!("{:?}", doc);
+
+    // Index access for map & array
+    //assert_eq!(doc["foo"][0].as_str().unwrap(), "list1");
+    //assert_eq!(doc["bar"][1].as_f64().unwrap(), 2.0);
+
+    // Chained key/array access is checked and won't panic,
+    // return BadValue if they are not exist.
+    //assert!(doc["INVALID_KEY"][100].is_badvalue());
+
+    // Dump the YAML object
+    let mut out_str = String::new();
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(doc).unwrap(); // dump the YAML object to a String
+    }
+    println!("{}", out_str);
+}
+
+fn delete_bookmark<'a>(name: String) -> &'a str
+{
+    return "Delete called with parameter";
+}
+
+fn add_bookmark<'a>(name: String) -> &'a str
+{
+    return "Add called";
+}
+
 fn main() {
     let args = Args::parse();
 
+    // First we will need to load the YAML. Let's simualte it
+    // with a variable for now:
+    let bm_yaml =
+	"
+bm: /home/gthvn1/rust_kindergarten/bm
+another: /home/gthvn1/another_dir
+";
+
+    let docs = YamlLoader::load_from_str(bm_yaml).unwrap();
+
+    // Multi document support, doc is a yaml::Yaml
+    let doc = &docs[0];
+    print_type_of(&doc);
+
     match (args.list, args.delete, args.add) {
-	(true, None, None) => println!("List is selected"),
-	(false, Some(s), None) => println!("Delete: <{}>", s),
-	(false, None, Some(s)) => println!("Add   : <{}>", s),
-	_ => println!("By default we list all bookmarks"),
+	(false, Some(s), None) => println!("<{}>", delete_bookmark(s)),
+	(false, None, Some(s)) => println!("<{}>", add_bookmark(s)),
+	// By default we list all bookmarks
+	_ => list_bookmarks(&doc),
     }
+
+
+
 }
