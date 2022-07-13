6725c0f6f39820e49f7c06a8c691b67f9ff322aa
Guillaume <gthouvenin@kalray.eu>
2022-07-13 13:16:32 +0200

    sothe1: Add the autogenerated code

--- codingame/shadows-of-the-knight-episode-1/src/main.rs
+++ codingame/shadows-of-the-knight-episode-1/src/main.rs
@@ -1,3 +1,40 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 fn main() {
-    println!("Hello, world!");
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let w = parse_input!(inputs[0], i32); // width of the building.
+    let h = parse_input!(inputs[1], i32); // height of the building.
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let n = parse_input!(input_line, i32); // maximum number of turns before game over.
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let x0 = parse_input!(inputs[0], i32);
+    let y0 = parse_input!(inputs[1], i32);
+
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let bomb_dir = input_line.trim().to_string(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+        // the location of the next window Batman should jump to.
+        println!("0 0");
+    }
 }
