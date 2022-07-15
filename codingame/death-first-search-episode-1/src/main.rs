1365d7d19c4428e8fe1ed6fc36904d2b89c8b45b
Guillaume <gthouvenin@kalray.eu>
2022-07-15 15:10:41 +0200

    dfse1: Start by creating simple graph
    Just create some nodes, add edges and print the graph.

--- codingame/death-first-search-episode-1/src/main.rs
+++ codingame/death-first-search-episode-1/src/main.rs
@@ -1,3 +1,35 @@
+#[derive(Debug, Default)]
+struct Node {
+    neighbours: Vec<i32>,
+    visited: bool,
+}
+
+type Graph = Vec<Node>;
+
 fn main() {
-    println!("Hello, world!");
+    let mut g: Graph = Graph::new();
+
+
+    // Start with simple graph
+    //     0
+    //    / \
+    //   1   2
+    //  /
+    // 3
+
+    // Start by creating the four nodes
+    g.push(Node::default());
+    g.push(Node::default());
+    g.push(Node::default());
+    g.push(Node::default());
+
+    // Add the edges
+    g[0].neighbours.push(1);
+    g[0].neighbours.push(2);
+    g[1].neighbours.push(3);
+
+    // Print the graph
+    for (i, n) in g.iter().enumerate() {
+        println!("Node {}: {:?}", i, n);
+    }
 }
