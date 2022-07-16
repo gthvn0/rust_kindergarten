bfc3782686571872b36241920d39c92f40042a0d
Guillaume <gthouvenin@kalray.eu>
2022-07-16 12:21:39 +0200

    dfse1: Add nodes automatically when needed
    Add a node only if dst or src are not in the range of the graph. Nodes
    are numeroted in order 0, 1, ... If we want to use others names like
    "node1" we will need to add a hash map that will do the translation
    between the node name and its value in the graph. But currently it is ok
    to use 0, 1, ...

--- codingame/death-first-search-episode-1/src/main.rs
+++ codingame/death-first-search-episode-1/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 #[derive(Debug, Default)]
 struct Node {
     neighbours: Vec<usize>,
@@ -10,41 +12,43 @@ struct Graph {
 }
 
 impl Graph {
-    // Return a new graph with n nodes
-    fn new(n: usize) -> Self {
-        let mut g: Graph = Graph::default();
-
-        for _ in 0..n {
-            g.nodes.push(Node::default());
-        }
-        g
-    }
-
     fn add_edge(&mut self, src: usize, dst: usize) {
+        let m: usize = max(src, dst) + 1; // used to check if new nodes need to be created
+
+        // If len is greater or equla to m nothing is pushed
+        for _ in self.nodes.len()..m {
+            self.nodes.push(Node::default());
+        }
         self.nodes[src].neighbours.push(dst);
     }
 
     fn display(&self) {
         for (i, n) in self.nodes.iter().enumerate() {
             println!("Node {}: {:?}", i, n);
-        } 
+        }
     }
 }
+
 fn main() {
     // Start with simple graph
-    //     0
-    //    / \
-    //   1   2
-    //  /
-    // 3
+    //      0
+    //     / \
+    //    1   2
+    //   /|   |\
+    //  / 4---5 \
+    // 3         6
+    // Create a graph
+    let mut g: Graph = Graph::default();
 
-    // Create a graph with four nodes
-    let mut g: Graph = Graph::new(4);
-
-    // Add the edges
+    // Add some edges
     g.add_edge(0, 1);
     g.add_edge(0, 2);
     g.add_edge(1, 3);
+    g.add_edge(1, 4);
+    g.add_edge(2, 5);
+    g.add_edge(2, 6);
+    g.add_edge(4, 5);
+
 
     g.display();
 }
