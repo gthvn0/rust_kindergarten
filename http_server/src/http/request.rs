4a97e446990058102ceeb98bb14095a99959feeb
gthvn0 <gthvn0@gmail.com>
2022-12-24 15:33:56 +0100

    http_server: add a custom ParseError

--- http_server/src/http/request.rs
+++ http_server/src/http/request.rs
@@ -1,4 +1,6 @@
 use super::method::Method;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 pub struct Request {
     path: String,
@@ -7,9 +9,41 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         unimplemented!();
     }
 }
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid request",
+            Self::InvalidEncoding => "Invalid encoding",
+            Self::InvalidProtocol => "Invalid protocol",
+            Self::InvalidMethod => "Invalid method",
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
