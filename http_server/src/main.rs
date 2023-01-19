bd0c887ab511906d1e06ec372b7b095cf3967632
Guillaume <gthvn1@gmail.com>
2023-01-19 17:10:10 +0100

    server: add public path

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -2,17 +2,19 @@
 
 use server::Server;
 use website_handler::WebSiteHandler;
+use std::env;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
-    // We want to pass the IP and the port
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server = Server::new("127.0.0.1:8080".to_string());
 
     // will run forever and wait for request
-    server.run(WebSiteHandler);
+    server.run(WebSiteHandler::new(public_path));
 }
 
 /* sample of a request
--- http_server/src/website_handler.rs
+++ http_server/src/website_handler.rs
@@ -4,7 +4,15 @@ use super::http::Response;
 use super::http::StatusCode;
 use super::server::Handler;
 
-pub struct WebSiteHandler;
+pub struct WebSiteHandler {
+    public_path: String,
+}
+
+impl WebSiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+}
 
 impl Handler for WebSiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
