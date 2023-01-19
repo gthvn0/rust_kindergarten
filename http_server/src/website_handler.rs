f314593b1551e1ff00aed3a331cd4722f7d4871d
Guillaume <gthvn1@gmail.com>
2023-01-19 16:10:38 +0100

    server: add /hello route

--- http_server/src/website_handler.rs
+++ http_server/src/website_handler.rs
@@ -9,10 +9,11 @@ pub struct WebSiteHandler;
 impl Handler for WebSiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
-            Method::GET => Response::new(
-                StatusCode::Ok,
-                Some("<h1>Test web site handler</h1>".to_string()),
-            ),
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
             _ => Response::new(StatusCode::NotFound, None),
         }
     }
