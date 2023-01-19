724c4a1f5ca78aca51b029e85f944018b8b64cd7
Guillaume <gthvn1@gmail.com>
2023-01-19 16:03:05 +0100

    server: start routing in website handler

--- http_server/src/website_handler.rs
+++ http_server/src/website_handler.rs
@@ -1,14 +1,19 @@
 use super::server::Handler;
-use super::http::response::Response;
-use super::http::request::Request;
-use super::http::status_code::StatusCode;
+use super::http::Response;
+use super::http::Request;
+use super::http::StatusCode;
+use super::http::Method;
 
 pub struct WebSiteHandler;
 
 impl Handler for WebSiteHandler {
-	fn handle_request(&mut self, _request: &Request) -> Response {
-		Response::new(
-			StatusCode::Ok,
-			Some("<h1>Test web site handler</h1>".to_string()))
+	fn handle_request(&mut self, request: &Request) -> Response {
+		match request.method() {
+			Method::GET =>
+				Response::new(
+					StatusCode::Ok,
+					Some("<h1>Test web site handler</h1>".to_string())),
+			_ => Response::new(StatusCode::NotFound, None)
+		}
 	}
 }
