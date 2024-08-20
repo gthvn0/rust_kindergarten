0217183838ce960b890747ea32f943ca44141057
Guillaume <guillaume.thouvenin@vates.tech>
2024-08-20 20:55:26 +0200

    add an export function

--- rust_to_wasm/wasm_module.rs
+++ rust_to_wasm/wasm_module.rs
@@ -6,7 +6,17 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+extern "C" {
+    fn loggme(msg: *const u8, len: i32);
+}
+
 #[no_mangle]
 pub fn a_sum(a: i32, b: i32) -> i32 {
+    let msg = "The answer is";
+    let msg_len = msg.len() as i32;
+
+    unsafe {
+        loggme(msg.as_ptr(), msg_len);
+    }
     a + b
 }
