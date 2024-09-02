1e0e1d560d8b9182f2020a16eb31426ec59dc9b1
Guillaume <guillaume.thouvenin@vates.tech>
2024-09-02 18:41:27 +0200

    add nbdkit plugin in Rust

--- nbdkit/README.md
+++ nbdkit/README.md
@@ -1,5 +1,7 @@
 # Write a nbdkit plugin in Rust
 
+- [nbdkit-plugin](https://libguestfs.org/nbdkit-plugin.3.html)
+
 - Try it in C
   - We will create a plugin that will only write "hello" and that is backed by memory
   - To build the plugin: `make`
@@ -12,4 +14,13 @@
     - Now read the first bytes: `dd if=/dev/nbd0 of=./read.bin bs=16 count=1`
     - Check that it is working: `hexdump -C ./read.bin`
     - deconnect: `sudo nbd-client -d /dev/nbd0`
+
 - Then in Rust
+  - It creates an empty disk with a specific header...
+  - In the Rust implementation if everything works as expected you should be able to read the disk using `dd` and see:
+  ```
+  ❯ hexdump -C read.bin
+00000000  48 65 6c 6c 6f 2c 20 53  61 69 6c 6f 72 21 00 00  |Hello, Sailor!..|
+00000010  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+00000020
+```
---
+++ nbdkit/rust_plugin/myplugin.rs
@@ -0,0 +1,54 @@
+use lazy_static::lazy_static;
+use nbdkit::*;
+use std::sync::Mutex;
+
+// The disk.
+lazy_static! {
+    static ref SIZE: usize = 1024 * 1024; // 1Mo
+    static ref DISK: Mutex<Vec<u8>> = Mutex::new(vec![0; 0]);
+}
+
+#[derive(Default)]
+struct MemoryDisk {
+    _dummy: u8, // it is needed otherwise Box::new won't allocate anything...
+}
+
+impl Server for MemoryDisk {
+    fn name() -> &'static str {
+        "memorydisk"
+    }
+
+    fn open(_readonly: bool) -> Result<Box<dyn Server>> {
+        // Write the header "Hello, Sailor!" at the beginning of the disk.
+        let header = b"Hello, Sailor!";
+        DISK.lock().unwrap()[..header.len()].copy_from_slice(header);
+
+        Ok(Box::<MemoryDisk>::default())
+    }
+
+    fn get_size(&self) -> Result<i64> {
+        Ok(DISK.lock().unwrap().len() as i64)
+    }
+
+    fn read_at(&self, buf: &mut [u8], offset: u64) -> Result<()> {
+        let disk = DISK.lock().unwrap();
+        let ofs = offset as usize;
+        let end = ofs + buf.len();
+        buf.copy_from_slice(&disk[ofs..end]);
+        Ok(())
+    }
+
+    fn thread_model() -> Result<ThreadModel> {
+        Ok(ThreadModel::SerializeConnections)
+    }
+
+    fn get_ready() -> Result<()> {
+        *DISK.lock().unwrap() = vec![0; *SIZE];
+        Ok(())
+    }
+}
+
+plugin!(MemoryDisk {
+    thread_model,
+    get_ready
+});
